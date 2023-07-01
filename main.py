8f1cbb48b0a04939878ca31be7b4f8a1dcfbce86
Natalia Chizhova <volmier@yandex.ru>
2023-07-01 10:26:57 +0300

    first chat bot

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print('y')
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+API_CATS_URL: str = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN: str = '5822431030:AAH0DsNkD1COsdRnlXTGSq22a5f-vzHUOIc'
+TEXT: str = 'Здесь должна была быть картинка с котиком :('
+TEXT_CAT: str  = 'Котиков мало не бывает'
+
+offset: int = -2
+counter: int = 0
+cat_response: requests.Response
+cat_link: str
+
+
+while counter < 100:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT_CAT}')
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
+
